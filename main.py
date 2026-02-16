3725c99d6a025b44ddb36ae7c61a6056e42ecd5e
basilbenny1002 <basilbenny1002@gmail.com>
2026-02-16 20:45:22 +0530

    Implement websocket for teh front end

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
-from routers.cart.websocket import router as websocket_router
+from routers.cart.websocket import router as cart_websocket_router
+from routers.frontend.websocket import router as frontend_websocket_router
 
 app = FastAPI()
 
-# Include the cart WebSocket router
-app.include_router(websocket_router)
+# Include the WebSocket routers
+app.include_router(cart_websocket_router)
+app.include_router(frontend_websocket_router)
