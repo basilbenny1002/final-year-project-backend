d6900155281f049ad32a42dd6b02e5a709577eb0
basilbenny1002 <basilbenny1002@gmail.com>
2026-02-17 12:38:18 +0530

    Add a health check endpoint to the base url

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ from routers.frontend.websocket import router as frontend_websocket_router
 
 app = FastAPI()
 
+
+@app.get("/")
+def base_status():
+	return {"status": "ok", "message": "ITS WORKING!!", "version": "1.0.0"}
+
 # Include the WebSocket routers
 app.include_router(cart_websocket_router)
 app.include_router(frontend_websocket_router)
