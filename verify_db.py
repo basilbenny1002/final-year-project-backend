7e963df9a55dd5f1c4983868a6d51dea2343de2b
basilbenny1002 <basilbenny1002@gmail.com>
2026-02-17 12:24:56 +0530

    Remove verification db code

--- verify_db.py
+++
@@ -1,9 +0,0 @@
-from database.products import init_db, get_product
-
-if __name__ == "__main__":
-    # init_db()
-    try:
-        product = get_product(100)
-        print(f"Product fetched: {product}")
-    except Exception as e:
-        print(f"Error: {e}")
